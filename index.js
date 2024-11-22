4e42d43e772caa4cf39a8a2697f853422dae1355
ahmed-jobayer <jobayer320721@.com>
2024-11-22 20:14:51 +0600

    product get api created

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const jwt =require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 const app = express();
@@ -19,18 +19,18 @@ app.use(express.json());
 
 const verifyJWT = (req, res, next) => {
   const authorization = req.headers.authorization;
-  if (authorization) {
-    return res.send({message: 'No Token'})
+  if (!authorization) {
+    return res.send({ message: "No Token" });
   }
-  const token = authorization.split(' ')[1]
+  const token = authorization.split(" ")[1];
   jwt.verify(token, process.env.ACCESS_KEY_TOKEN, (error, decoded) => {
     if (error) {
-      return res.send({message: 'Invalid Token'})
+      return res.send({ message: "Invalid Token" });
     }
-    req.decoded = decoded
-    next()
-  })
-}
+    req.decoded = decoded;
+    next();
+  });
+};
 
 // /mongodb
 
@@ -44,8 +44,8 @@ const client = new MongoClient(url, {
   },
 });
 
-const userCollection = client.db('UrbanNest').collection('users')
-const productCollection = client.db('UrbanNest').collection('products')
+const userCollection = client.db("UrbanNest").collection("users");
+const productCollection = client.db("UrbanNest").collection("products");
 
 const dbConnect = async () => {
   try {
@@ -54,18 +54,75 @@ const dbConnect = async () => {
 
     // post user
 
-  app.post('/user', async (req, res) => {
-    const user = req.body;
-    const existingUser = await userCollection.findOne({ email: user.email });
+    app.post("/user", async (req, res) => {
+      const user = req.body;
+      const existingUser = await userCollection.findOne({ email: user.email });
 
-    if (existingUser) {
-      return res.send({ message: "User already exists" });
-    }
+      if (existingUser) {
+        return res.send({ message: "User already exists" });
+      }
 
-    const result = await userCollection.insertOne(user);
-    res.send(result);
-  })
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
 
+    // get single user
+
+    app.get("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      console.log(email)
+      const query = { email: email };
+      const result = await userCollection.findOne(query);
+      res.send(result);
+      console.log(result);
+    });
+
+// get product
+
+app.get("/all-products", async (req, res) => {
+  // name searching
+  // sort by price
+  // filter by category
+  // filter by brand
+
+  const { title, sort, category, brand, page = 1, limit = 9 } = req.query;
+
+  const query = {};
+
+  if (title) {
+    query.title = { $regex: title, $options: "i" };
+  }
+
+  if (category) {
+    query.category = { $regex: category, $options: "i" };
+  }
+
+  if (brand) {
+    query.brand = brand;
+  }
+
+  const pageNumber = Number(page);
+  const limitNumber = Number(limit);
+
+  const sortOptions = sort === "asc" ? 1 : -1;
+
+  const products = await productCollection
+    .find(query)
+    .skip((pageNumber - 1) * limit)
+    .limit(limitNumber)
+    .sort({ price: sortOptions })
+    .toArray();
+
+  const totalproducts = await productCollection.countDocuments(query);
+
+  const categories = [
+    ...new Set(products.map((product) => product.category)),
+  ];
+
+  const brands = [...new Set(products.map((product) => product.brand))];
+
+  res.json({ products, totalproducts, categories, brands });
+});
 
 
 
@@ -82,21 +139,20 @@ const dbConnect = async () => {
 
 dbConnect();
 
-
 app.get("/", (req, res) => {
   res.send("Server is running");
 });
 
-app.post('/authentication', async (req, res) => {
-  const userEmail = req.body
+app.post("/authentication", async (req, res) => {
+  const userEmail = req.body;
+  // console.log(userEmail)
   const token = jwt.sign(userEmail, process.env.ACCESS_KEY_TOKEN, {
-    expiresIn: '10d',
-  })
-  res.send(token)
-})
-
+    expiresIn: "10d",
+  });
+  // console.log(token)
+  res.send(token);
+});
 
 app.listen(port, () => {
   console.log(`Server is runing on port, ${port}`);
 });
-
