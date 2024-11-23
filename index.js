abe5a899a21e979382e56e5ea572ad2ad72105dd
ahmed-jobayer <jobayer320721@.com>
2024-11-23 21:46:21 +0600

    add to wishlist api created

--- index.js
+++ index.js
@@ -36,12 +36,12 @@ const verifyJWT = (req, res, next) => {
 
 const verifySeller = async (req, res, next) => {
   const email = req.decoded.email;
-  const user = await userCollection.findOne({email: email})
-  if (user?.role !== 'seller') {
-    return res.send({message: 'Forbidden Access'})
+  const user = await userCollection.findOne({ email: email });
+  if (user?.role !== "seller") {
+    return res.send({ message: "Forbidden Access" });
   }
-  next()
-}
+  next();
+};
 
 // /mongodb
 
@@ -122,25 +122,59 @@ const dbConnect = async () => {
       const product = req.body;
       // console.log(product)
       const result = await productCollection.insertOne(product);
-      console.log(result)
+      console.log(result);
       // res.send(result);
     });
 
     // get items according to seller email
 
-    app.get('/seller-products', verifyJWT, verifySeller, async(req, res) => {
-      const email = req.query.email
-      const result = await productCollection.find({SellerEmail: email}).toArray()
-      res.send(result)
-    })
-// delet item with id
-    app.delete("/deleteProduct/:id", verifyJWT, verifySeller, async (req, res) => {
-      const id = req.params.id;
-      console.log(id)
-      const result = await productCollection.deleteOne({
-        _id: new ObjectId(String(id)),
-      });
+    app.get("/seller-products", verifyJWT, verifySeller, async (req, res) => {
+      const email = req.query.email;
+      const result = await productCollection
+        .find({ SellerEmail: email })
+        .toArray();
+      res.send(result);
+    });
+    // delet item with id
+    app.delete(
+      "/deleteProduct/:id",
+      verifyJWT,
+      verifySeller,
+      async (req, res) => {
+        const id = req.params.id;
+        console.log(id);
+        const result = await productCollection.deleteOne({
+          _id: new ObjectId(String(id)),
+        });
+        console.log(result);
+        res.send(result);
+      }
+    );
+
+    // only users api
+    // add to wishlist
+    app.patch("/add-to-wishlist/:id", verifyJWT, async (req, res) => {
+      const productId = req.params.id;
+      const userEmail = req.decoded.email;
+      const query = { email: userEmail };
+      const updateDoc = {
+        $addToSet: { wishlist: productId },
+      };
+      const result = await userCollection.updateOne(query, updateDoc);
+      res.send(result);
       console.log(result)
+    });
+
+    // update a product
+
+    app.patch("/updateProduct/:id", async (req, res) => {
+      const id = req.params.id;
+      // console.log(id)
+
+      const query = { _id: new ObjectId(String(id)), role: "buyer" };
+      const updatedDoc = { $set: { role: "seller" } };
+
+      const result = await productCollection.updateOne(query, updatedDoc);
       res.send(result);
     });
 
